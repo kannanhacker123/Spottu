6020b6820b2c1e84505b2f3c81d828163810e2dc
Kannan <kannan.hacker1@gmail.com>
2024-12-08 12:44:44 +0530

    all are admins

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -4,7 +4,7 @@ import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.use(protectRoute, requireAdmin);
+router.use(protectRoute);
 
 router.get("/check", checkAdmin);
 
