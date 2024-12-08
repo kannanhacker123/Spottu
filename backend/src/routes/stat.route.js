1f79479408494ba15ecf29a78e95c84c95027c0e
Kannan <kannan.hacker1@gmail.com>
2024-12-08 13:08:30 +0530

    anyone can see the stats

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -5,6 +5,7 @@ import { getStats } from "../controller/stat.controller.js";
 const router = Router();
 
 // protect all routes no matter what no need of requireAdmin
+// anyone can see the stats
 router.get("/", protectRoute, getStats);
 
 export default router;
